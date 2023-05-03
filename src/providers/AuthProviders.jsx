0f7056e1b5f2b8a20cd4000b558c1b64bfda0aaf
yeasin550 <yeasin51570@gmail.com>
2023-05-03 10:50:32 +0600

    create auth context and google login

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -4,7 +4,7 @@ import app from './../firebase/firebase.config';
 
 export const AuthContext = createContext()
 const AuthProviders = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
   const auth = getAuth(app)
   const googleProvider = new GoogleAuthProvider();
   const githubProvider = new GithubAuthProvider();
